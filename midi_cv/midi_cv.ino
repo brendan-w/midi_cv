fd4100f531ffdf094150d60136c5c31379e3895c
Brendan Whitfield <brendanw@windworksdesign.com>
2022-12-10 18:34:26 -0800

    Switched to transfer16() to fix endian

--- midi_cv/midi_cv.ino
+++ midi_cv/midi_cv.ino
@@ -2,7 +2,6 @@
 #include <SPI.h>
 
 #include "src/Array/src/Array.h"  // https://github.com/janelia-arduino/Array because I miss the STL too much
-#include "src/Arduino_Endian/src/endian.h"  // https://github.com/lathoub/Arduino_Endian
 
 const int MCP_LDAC = 9;
 const Array<int, 4> CHANNEL_SELECTOR_SWITCH_PINS({A0, A1, A2, A3});
@@ -13,33 +12,32 @@ const Array<int, 4> CHANNEL_SELECTOR_SWITCH_PINS({A0, A1, A2, A3});
 // of the shift register chain. Note that the MCP4812 can also only update one channel at a time,
 // so it takes two of these commands to update all three outputs. This is slightly inefficient
 // (we're updating the LTC twice), but not noticable when running at >= 1MHz baudrates.
-struct DACCommand {
-    // Command for the MCP4812
-    // ----------------------------------
-    uint8_t mcp_ab_select:1;
-    static constexpr uint8_t MCP_DAC_A = 0;
-    static constexpr uint8_t MCP_DAC_B = 1;
+union MCP4812Command {
+    struct Fields {
+        // Command for the MCP4812
+        // ----------------------------------
+        uint8_t mcp_ab_select:1;
+        static constexpr uint8_t MCP_DAC_A = 0;
+        static constexpr uint8_t MCP_DAC_B = 1;
 
-    uint8_t _:1;  // unused
+        uint8_t _:1;  // unused
 
-    uint8_t mcp_gain_select:1;
-    static constexpr uint8_t MCP_GAIN_2X = 0;
-    static constexpr uint8_t MCP_GAIN_1X = 1;
+        uint8_t mcp_gain_select:1;
+        static constexpr uint8_t MCP_GAIN_2X = 0;
+        static constexpr uint8_t MCP_GAIN_1X = 1;
 
-    uint8_t mcp_output_shutdown:1;
-    static constexpr uint8_t MCP_OUTPUT_ACTIVE = 1;
-    static constexpr uint8_t MCP_OUTPUT_SHUTDOWN = 0;
+        uint8_t mcp_output_shutdown:1;
+        static constexpr uint8_t MCP_OUTPUT_ACTIVE = 1;
+        static constexpr uint8_t MCP_OUTPUT_SHUTDOWN = 0;
 
-    uint16_t mcp_value:10;
+        uint16_t mcp_value:10;
 
-    uint8_t __:2;  // unused
+        uint8_t __:2;  // unused
+    } __attribute__((packed)) fields;
+    uint16_t value;
+};
 
-    // Command for the LTC1655IN8
-    // ----------------------------------
-    uint16_t ltc_value;
-} __attribute__((packed));
-
-static_assert(sizeof(DACCommand) == 4);
+static_assert(sizeof(MCP4812Command) == 2);
 
 
 void update_dacs(uint16_t ltc, uint16_t mcp_a, uint16_t mcp_b) {
@@ -47,12 +45,24 @@ void update_dacs(uint16_t ltc, uint16_t mcp_a, uint16_t mcp_b) {
     mcp_a = min(mcp_a, 1023);
     mcp_b = min(mcp_b, 1023);
 
-    DACCommand command = {
-        mcp_ab_select: DACCommand::MCP_DAC_A,
-        mcp_gain_select: DACCommand::MCP_GAIN_2X,
-        mcp_output_shutdown: DACCommand::MCP_OUTPUT_ACTIVE,
-        mcp_value: mcp_a,
-        ltc_value: htobe16(ltc),
+    // NOTE: These structs are overwritten with input data by the SPI.transfer API,
+    // so we prepare two of them rather than re-using one.
+    MCP4812Command command_a = {
+        fields: {
+            mcp_ab_select: MCP4812Command::Fields::MCP_DAC_A,
+            mcp_gain_select: MCP4812Command::Fields::MCP_GAIN_2X,
+            mcp_output_shutdown: MCP4812Command::Fields::MCP_OUTPUT_ACTIVE,
+            mcp_value: mcp_a,
+        }
+    };
+
+    MCP4812Command command_b = {
+        fields: {
+            mcp_ab_select: MCP4812Command::Fields::MCP_DAC_B,
+            mcp_gain_select: MCP4812Command::Fields::MCP_GAIN_2X,
+            mcp_output_shutdown: MCP4812Command::Fields::MCP_OUTPUT_ACTIVE,
+            mcp_value: mcp_b,
+        }
     };
 
     // Slow down the SPI clock to <= 500khz
@@ -60,17 +70,13 @@ void update_dacs(uint16_t ltc, uint16_t mcp_a, uint16_t mcp_b) {
     SPI.beginTransaction(settings); // Configures SPI and disables interrupts
 
     digitalWrite(SS, LOW);
-    SPI.transfer(reinterpret_cast<uint8_t*>(&command), sizeof(command));
+    SPI.transfer16(command_a.value);
+    SPI.transfer16(ltc);
     digitalWrite(SS, HIGH);
-
-    // Populate the MCP's B-side register
-    command.mcp_ab_select = DACCommand::MCP_DAC_B;
-    command.mcp_value = mcp_b;
-    // Leave everything else the same
-
     // digitalWrite() is slow enough that its actually been 5usec since we set it high
     digitalWrite(SS, LOW);
-    SPI.transfer(reinterpret_cast<uint8_t*>(&command), sizeof(command));
+    SPI.transfer16(command_b.value);
+    SPI.transfer16(ltc);
     digitalWrite(SS, HIGH);
 
     // Trigger the MCP to load/output the new value
